97ebd8c54a9722fac15676ee521e25290109c814
Daniel Zarate <d.ansizgz@gmail.com>
2025-03-07 13:42:07 -0500

    :hammer: Remove unused mailer and model classes; update application configuration

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -5,6 +5,7 @@ require "action_controller/railtie"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_support/railtie"
+require "action_cable/engine"  
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
